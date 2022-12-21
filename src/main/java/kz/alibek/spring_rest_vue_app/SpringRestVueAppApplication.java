3109699570a39fa5eba04c9de2710f6d1d316cdd
Alibek <venommega20012@gmail.com>
2022-12-21 15:59:17 +0600

    initialized project

---
+++ src/main/java/kz/alibek/spring_rest_vue_app/SpringRestVueAppApplication.java
@@ -0,0 +1,13 @@
+package kz.alibek.spring_rest_vue_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestVueAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestVueAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/alibek/spring_rest_vue_app/SpringRestVueAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.alibek.spring_rest_vue_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestVueAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
