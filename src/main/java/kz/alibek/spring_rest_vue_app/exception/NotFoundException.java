f6bb7e4fa825d16532990956e6d86c150c55e25f
Alibek <venommega20012@gmail.com>
2022-12-21 16:19:29 +0600

    added controller + exception handler

---
+++ src/main/java/kz/alibek/spring_rest_vue_app/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package kz.alibek.spring_rest_vue_app.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+}
